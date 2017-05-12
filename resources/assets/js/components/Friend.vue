e372c80d892bfbea96f966dee4fd0a09df05d08a
Вадим Стецюк < >
2017-05-12 14:02:07 +0300

    Initial commit

---
+++ resources/assets/js/components/Friend.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="row">
+            This is new component
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            this.$http.get('/check_relationship_status/' + this.profile_user_id)
+                .then((resp) => {
+                    console.log(resp)
+                })
+        },
+        props: ['profile_user_id'],
+
+    }
+</script>
